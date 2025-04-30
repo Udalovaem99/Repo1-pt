9d62d019dfac66758c6033b49c3c7837e3008d04
Udalovaem99 <cananaaska1512@gmail.com>
2025-04-30 10:03:54 +0400

    Update praktika1.py
    Практика к 1ДЗ

--- praktika1.py
+++ praktika1.py
@@ -1,22 +1,22 @@
-#my_heigh = 163
-#print(my_heigh)
+my_heigh = 163
+print(my_heigh)
 
-#my_name = "Катя"
-#my_name = "Удалова Екатерина"
-#print(my_name)
+my_name = "Катя"
+my_name = "Удалова Екатерина"
+print(my_name)
 
-#pet_name = "Зевс"   
-#pet_name = input("Как зовут вашего питомца? ")
-#print("Ваш любимчик - "+ pet_name)
+pet_name = "Зевс"   
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - "+ pet_name)
 
-#def print_python():
-#    print ("Учу Python!")
-#print_python ()
+def print_python():
+    print ("Учу Python!")
+print_python ()
 
-#def my_name(let):
-#    print(let,end='')
+def my_name(let):
+    print(let,end='')
 
-#my_name('К')
-#my_name('А')
-#my_name('Т')
-#my_name('Я')
+my_name('К')
+my_name('А')
+my_name('Т')
+my_name('Я')
